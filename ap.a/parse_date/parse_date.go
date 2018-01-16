cc6f3f296f6244eb8ff067ef9b88cbddc79ed794
Yuichi Tanikawa <kojole.jp@gmail.com>
2018-01-16 11:55:02 +0900

    Read appendix A

---
+++ ap.a/parse_date/parse_date.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+type DueDate struct {
+	time.Time
+}
+
+func (d *DueDate) UnmarshalJSON(raw []byte) error {
+	epoch, err := strconv.Atoi(string(raw))
+	if err != nil {
+		return err
+	}
+	d.Time = time.Unix(int64(epoch), 0)
+	return nil
+}
+
+type Todo struct {
+	Task string  `json:"task"`
+	Time DueDate `json:"due"`
+}
+
+var jsonString = []byte(`
+[
+	{"task": "幼稚園登園", "due": 1486600200},
+	{"task": "エリクソン研究会に行く", "due": 1486634400}
+]
+`)
+
+func main() {
+	var todos []Todo
+	err := json.Unmarshal(jsonString, &todos)
+	if err != nil {
+		panic(err)
+	}
+	for _, todo := range todos {
+		fmt.Printf("%s: %v\n", todo.Task, todo.Time)
+	}
+}
